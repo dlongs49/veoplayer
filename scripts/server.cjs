e97eb5deb97c4098150e498b102c3abf877fbe7f
dinglong <dillonl.dl49@gmail.com>
2023-06-25 18:14:41 +0800

    fix:#修改保留字

--- scripts/server.cjs
+++ scripts/server.cjs
@@ -1,8 +1,8 @@
 const Koa = require('koa');
-const static = require('koa-static');
+const koaStatic = require('koa-static');
 const path = require('path')
 const app = new Koa()
-app.use(static(path.resolve(__dirname, "../"), {extensions: ["html"]}))
+app.use(koaStatic(path.resolve(__dirname, "../"), {extensions: ["html"]}))
 
 app.listen(3000, () => {
     console.log("koa服务启动:http:127.0.0.1:3000")
