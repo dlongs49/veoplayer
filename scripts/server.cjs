1a37325b7cc8a74b745e1eef711f27f6188f230f
dinglong <dillonl.dl49@gmail.com>
2023-06-27 18:58:36 +0800

    cli:#构建工具

--- scripts/server.cjs
+++
@@ -1,9 +0,0 @@
-const Koa = require('koa');
-const koaStatic = require('koa-static');
-const path = require('path')
-const app = new Koa()
-app.use(koaStatic(path.resolve(__dirname, "../"), {extensions: ["html"]}))
-
-app.listen(3000, () => {
-    console.log("koa服务启动:http:127.0.0.1:3000")
-})
