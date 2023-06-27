12d73d0c78a9147fb35e0e2f65d162249fa6cb2b
dinglong <dillonl.dl49@gmail.com>
2023-06-27 18:57:29 +0800

    fix:修改引入hls包

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,45 @@
 import { terser } from 'rollup-plugin-terser';
-import commonjs from "@rollup/plugin-commonjs";
-import postcss from "rollup-plugin-postcss";
-import autoprefixer from "autoprefixer"
-import cssnano from "cssnano";
+// import postcss from "rollup-plugin-postcss";
+// import autoprefixer from "autoprefixer"
+// import cssnano from "cssnano";
+import commonjs from '@rollup/plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+
+import nodePolyfills from 'rollup-plugin-node-polyfills'
 import strip from "@rollup/plugin-strip";
 import json from './package.json' assert {type: "json"}
 export default {
     input: "./packages/main.js",
     output: [{
-        file: `dist/${json.name}.cjs.js`,
-        format: "cjs",
-        name:json.name
-    }, {
-        file: `dist/${json.name}.esm.js`,
+        file: `dist/${json.name}.esm.min.js`,
         format: "esm",
-        name:json.name
+        name: json.name
     },
     {
-        file: `dist/${json.name}.global.js`,
+        file: `dist/${json.name}.global.min.js`,
         format: "iife",
-        name:json.name
+        name: json.name
     }],
     plugins: [
-        commonjs(),
-        terser(),
-        postcss({
-            plugins: [autoprefixer(), cssnano()],
+        babel({
+            exclude: "node_modules/**"
         }),
+        terser(),
+        // postcss({
+        //     plugins: [autoprefixer(), cssnano()],
+        // }),
+        commonjs(),
         strip(),
+        nodeResolve(),
+        livereload(),
+        nodePolyfills(),
+        serve({
+            port: 3000,
+            openPage: '/index.html',
+            contentBase: ''
+        })
     ]
 }
